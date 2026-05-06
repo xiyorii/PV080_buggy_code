fd8fdbda3655ba5538a57ff9b25d10a826bc3950
xiyorii <morales.armando2004@gmail.com>
2026-05-06 08:44:00 +0200

    issue 67: fixed undefined variable

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -29,7 +29,7 @@ def fetch_website(urllib_version, url):
     # Fetch and print the requested URL
  
     try: 
-        http = urllib.PoolManager()
+        http = urllib_version.PoolManager()
         r = http.request('GET', url)
     except:
         print('Exception')
