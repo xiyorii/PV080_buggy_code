2ef67aff35ac5d28935d3ecdfdc19ceb0f0422db
xiyorii <morales.armando2004@gmail.com>
2026-05-06 08:48:39 +0200

    issue 40: constant name changed to uppercase

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     print("4. Use of assert statements vulnerability:")
     choice  = input("Select vulnerability: ")
     if choice == "1": 
-        new_person = Person("Vickie")  
+        NEW_PERSON = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
         urlib_version = input("Choose version of urllib: ")
