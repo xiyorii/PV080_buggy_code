39ab36fc5402cc96425bdff2faa936537efb7534
xiyorii <morales.armando2004@gmail.com>
2026-05-06 09:01:26 +0200

    sql injections

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -25,7 +25,7 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    exec(f"import urllib{urllib_version} as urllib", globals()) # SQL injection, thank god for codeQL amirite
     # Fetch and print the requested URL
  
     try: 
